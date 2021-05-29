a137d90afbd030c01707f1a55bf47e204f9a6f2e
Alsu Shareeva <alsu1shareeva@gmail.com>
2021-05-30 01:56:28 +0300

    прокрутка и контент

--- main.js
+++ main.js
@@ -1,1 +1,82 @@
+const swiper = new Swiper('.channel-slider', {
+  // Optional parameters
 
+  loop: true,
+  slidesPerView: 1,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1400: {
+      slidesPerView: 5,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    760: {
+      slidesPerView: 2,
+    },
+  },
+  spaceBetween: 20,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+});
+
+const swiper2 = new Swiper('.recommended-slider', {
+  // Optional parameters
+
+  loop: true,
+  slidesPerView: 1,
+  breakpoints: {
+    1900: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 2,
+    },
+  },
+  spaceBetween: 40,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+});
+
+const swiper3 = new Swiper('.food-slider', {
+  // Optional parameters
+
+  loop: true,
+  slidesPerView: 1,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1400: {
+      slidesPerView: 5,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    760: {
+      slidesPerView: 2,
+    },
+  },
+  spaceBetween: 20,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.food-button-next',
+    prevEl: '.food-button-prev',
+  },
+});
